84944b8260808e8679c8fc732c94565a89a145a0
Andrew <36133342+Elkopasto@users.noreply.github.com>
2020-11-11 08:28:16 +0300

    Def - AddField
    * AddField draft

--- Main.py
+++ Main.py
@@ -1,3 +1,22 @@
 import sqlite3
 
-sqlite3.connect('Timetable.sqlite')
+
+def connect():
+    try:
+        return sqlite3.connect('Timetable.sqlite')
+    except Exception as e:
+        print(e)
+
+
+def addField(cur=None, **kwargs):
+    try:
+        cur.execute(f"INSERT INTO schedule ({', '.join(kwargs)}) VALUES ({', '.join(kwargs.values())})")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    con = connect()
+    cur = con.cursor()
+    addField(cur, time="'15:00'", subj="'maths'", grade="9", teacher="'Alexander'", room="'A1'", day="0")
+    con.commit()
